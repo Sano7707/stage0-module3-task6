2eb606e6baafd9ab28f6fca6be660290b2a95910
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-23 19:36:13 +0400

    Changes

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -3,10 +3,10 @@ package lang.print.gaps.task6;
 public class IntegerTypeCasting {
     public static void main(String[] args) {
         long first = 1000;
-        int second = 1000;
+        int second = (int)first;
         first = second;
-        System.out.println(first);
-        System.out.println(second);
+        System.out.println(first + "" + '\n' + second);
+
     }
 }
 
